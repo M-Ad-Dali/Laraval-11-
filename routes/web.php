1ed1310643c6eb6f21c4d3fd0bb4a15b9146a0e2
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-04 01:11:30 +0300

    route web

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use Pest\Support\View;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('home',[
+    'greeting' => 'Hello', /* [greeting = $greeting = Hello] */
+    'name' => 'MokaMo',
+    ]);
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+    });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+    Route::get('/jobs', function () {
+        return view('jobs',[
+    
+            'jobs' => [
+                [
+                    'id' => 1,
+                    'title' => 'Director',
+                    'salary' => '$50,000'
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Programmer',
+                    'salary' => '$10,000'
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Teacher',
+                    'salary' => '$40,000'
+                ]
+            ]
+        ]);
+    });
+
+    Route::get('/jobs/{id}', function ($id) {
+            $jobs = [
+                [
+                    'id' => 1,
+                    'title' => 'Director',
+                    'salary' => '$50,000'
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Programmer',
+                    'salary' => '$10,000'
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Teacher',
+                    'salary' => '$40,000'
+                ]
+            ];
+
+            $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+        // dd($job);
+
+            return view('job', ['job' => $job]);
+    });
