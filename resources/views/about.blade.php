96097d7b88758c88a42abea4b774f36c55e18e06
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-04 01:10:47 +0300

    about && contact

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <x-slot:heading>
+    About Pag
+  </x-slot:heading>
+  <h1>About Us</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <x-slot:heading>
+    Contact Pag
+  </x-slot:heading>
+  <h1>Contact Us</h1>
+</x-layout>
