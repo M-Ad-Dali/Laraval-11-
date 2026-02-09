7e49d4ce70089af683b322f133d13672884d7477
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-09 13:22:51 +0300

    modify the files

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -41,4 +41,9 @@ class Job extends Model{
         return $this->belongsTo(Employer::class); /* [تعريف العلاقة بين الوظيفة وصاحب العمل صاحب عمل واحد معه عدة وظائف] [One-to-Many] */
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id"); /* [تعريف العلاقة بين الوظيفة التاج وظيفة واحدة لها عدة تاجات والعكس صحيح] [Many-to-Many] */ /* [foreignPivotKey عشان تقدر تتعامل مع اسم مغاير للاسماء الاكفلر استخداماً في قواعد البيانات] */
+    }
+
 }
