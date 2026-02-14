878d250c449d08528a609c0c2c3e229ac002fe68
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-14 06:14:52 +0300

    fixed

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -1,22 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\job;
 
+use App\Models\job;
 use Illuminate\Http\Request;
-use Pest\Support\View;
 
 class JobController extends Controller
 {
     public function index()
     {
         $jobs = Job::with('employer')->latest()->simplePaginate(3); // [جلب ثلاثة بس في كل صفحة]
+
         // $jobs = job::all(); // [طريقة جلب البيانات من قاعدة البيانات بدون علاقة بين الجداول] [تحميل كسول للبيانات]
         // $jobs = Job::with('employer')->paginate(3); // [جلب ثلاثة بس في كل صفحة]
         // $jobs = Job::with('employer')->cursorPaginate(3); // [جلب ثلاثة بس في كل صفحة]
         // $jobs = Job::with('employer')->get() /* [with('employer'); = eager loading] */
         return view('jobs.index', [
-            'jobs' => $jobs
+            'jobs' => $jobs,
         ]);
     }
 
@@ -30,28 +30,29 @@ class JobController extends Controller
         return view('jobs.show', ['job' => $job]);
     }
 
-    public function stor()
+    public function store()
     {
         request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-]);
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
 
-    // $job = new Job(); // [طريقة 1]
+        // $job = new Job(); // [طريقة 1]
 
-    // $job->title = request('title'); 
-    // $job->salary = request('salary');
-    // $job->employer_id = 1;
+        // $job->title = request('title');
+        // $job->salary = request('salary');
+        // $job->employer_id = 1;
 
-    // $job->save();
+        // $job->save();
 
-    // dd(request()->all());// [dd = dump and die] [request()->all() = جلب كل البيانات اللي تم ارسالها من الفورم]
-    Job::create([ /* [طريقة 2] */
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-    return redirect('/jobs');
+        // dd(request()->all());// [dd = dump and die] [request()->all() = جلب كل البيانات اللي تم ارسالها من الفورم]
+        Job::create([/* [طريقة 2] */
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect('/jobs');
     }
 
     public function edit(Job $job)
@@ -63,24 +64,24 @@ class JobController extends Controller
     public function update(Job $job)
     {
         request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-]);
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
 
-    // $job = Job::findOrFail($id);  // [findOrFail = جلب البيانات أو إظهار خطأ 404 إذا لم يتم العثور عليها] null
+        // $job = Job::findOrFail($id);  // [findOrFail = جلب البيانات أو إظهار خطأ 404 إذا لم يتم العثور عليها] null
 
-    $job->update([ 
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
 
-    return redirect('/jobs/' . $job->id);
+        return redirect('/jobs/'.$job->id);
     }
 
     public function destroy(Job $job)
     {
         $job->delete(); // [delete = حذف البيانات]
 
-    return redirect('/jobs'); 
+        return redirect('/jobs');
     }
 }
