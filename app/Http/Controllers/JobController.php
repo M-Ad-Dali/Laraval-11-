555a581ff80e59731ff8690f2b61b0b5c408641f
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-14 04:55:06 +0300

    modify the files

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\job;
+
+use Illuminate\Http\Request;
+use Pest\Support\View;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->simplePaginate(3); // [جلب ثلاثة بس في كل صفحة]
+        // $jobs = job::all(); // [طريقة جلب البيانات من قاعدة البيانات بدون علاقة بين الجداول] [تحميل كسول للبيانات]
+        // $jobs = Job::with('employer')->paginate(3); // [جلب ثلاثة بس في كل صفحة]
+        // $jobs = Job::with('employer')->cursorPaginate(3); // [جلب ثلاثة بس في كل صفحة]
+        // $jobs = Job::with('employer')->get() /* [with('employer'); = eager loading] */
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function stor()
+    {
+        request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+]);
+
+    // $job = new Job(); // [طريقة 1]
+
+    // $job->title = request('title'); 
+    // $job->salary = request('salary');
+    // $job->employer_id = 1;
+
+    // $job->save();
+
+    // dd(request()->all());// [dd = dump and die] [request()->all() = جلب كل البيانات اللي تم ارسالها من الفورم]
+    Job::create([ /* [طريقة 2] */
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+    return redirect('/jobs');
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', ['job' => $job]);
+
+    }
+
+    public function update(Job $job)
+    {
+        request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+]);
+
+    // $job = Job::findOrFail($id);  // [findOrFail = جلب البيانات أو إظهار خطأ 404 إذا لم يتم العثور عليها] null
+
+    $job->update([ 
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+    }
+
+    public function destroy(Job $job)
+    {
+        $job->delete(); // [delete = حذف البيانات]
+
+    return redirect('/jobs'); 
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
   <x-slot:heading>
     Home Pag
   </x-slot:heading>
-  <h1>{{ $greeting }}... Welcome to the Home Page My Name Is {{ $name }}.</h1>
+  {{-- <h1>{{ $greeting }}... Welcome to the Home Page My Name Is {{ $name }}.</h1> --}}
 
 </x-layout>
