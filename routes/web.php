7d76438bd05147ead1930a505ef9e9bf526cf231
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-02-22 17:06:31 +0300

    modify the files policy files

--- routes/web.php
+++ routes/web.php
@@ -126,19 +126,47 @@ use App\Http\Controllers\SessionController;
 //     only => ['index', 'create', 'store', 'edit', 'update', 'destroy'] // [تحديد المسارات التي نريدها فقط]
 // ]);
 
+// Route::resource('jobs', JobController::class)->middleware('auth'); // [هذي الطريقة مستخدمة لختصار الكود عندما يكون في نفس الكلاس] [JobController::class = جلب كل المسارات من الكنترولر] [middleware('auth') = حماية جميع مسارات الوظائف من الوصول غير المصرح به]
+
 Route::view('/', 'home');
 
 Route::view('/about', 'about');
 
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class); // [هذي الطريقة مستخدمة لختصار الكود عندما يكون في نفس الكلاس] [JobController::class = جلب كل المسارات من الكنترولر]
+// Route::resource('jobs', JobController::class); // [هذي الطريقة مستخدمة لختصار الكود عندما يكون في نفس الكلاس] [JobController::class = جلب كل المسارات من الكنترولر]
 
+// Index
+Route::get('/jobs', [JobController::class, 'index']);
+
+// Create
+Route::get('/jobs/create', [JobController::class, 'create']);
+
+// Store
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth'); 
+
+// Show
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+
+// Edit
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth') // [حماية مسار تعديل الوظيفة من الوصول غير المصرح به]
+    ->can('edit', 'job'); // [can('edit-job', 'job') = التحقق من صلاحية المستخدم لتعديل الوظيفة اذا كانت توجد صلاحيات تظهر عدا ذالك تختفي]
+
+// Update
+Route::patch('/jobs/{job}', [JobController::class, 'update'])
+    ->middleware('auth')
+    ->can('update', 'job');
+
+// Destroy
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])
+    ->middleware('auth')
+    ->can('delete', 'job'); 
 // Auth
 Route::get('/register', [RegisterUserController::class, 'create']);
 Route::post('/register', [RegisterUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login'); // [تسمية المسار لتسهيل عملية إعادة التوجيه في حالة عدم تسجيل الدخول]
 Route::post('/login', [SessionController::class, 'store']);
 
 Route::post('/logout', [SessionController::class, 'destroy']);
